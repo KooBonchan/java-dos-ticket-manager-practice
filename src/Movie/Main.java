51465572c14dad55b6f8b240234ca2856c2c62e5
garam9koo <garam9koo@gmail.com>
2024-10-31 17:56:48 +0900

    init project

---
+++ src/Movie/Main.java
@@ -0,0 +1,27 @@
+package Movie;
+
+/*
+* ---------------------------------------------------
+* Writer: KooBonchan
+* Reference Project: MovieReservationReference Program
+* Start date: 241031
+* Project Structure
+* View - menu<Interface>
+*        menu<Abstract> - (MainMenu, AdminMenu)
+* Data - Reservation, Seats, Movie
+* Main  -ref-> (Reservation, Seats)
+* Movie -ref-> (Movie)
+*
+* IO   - (Reservation, Movie)
+*  as txt file, at project root directory
+*
+*
+*
+* */
+
+
+
+public class Main {
+  public static void main(String[] args) {
+  }
+}
